8e8bfa75f72778b87fff6583999bb1050fd63505
Shavkat Jo'raboyev <92091244+devpythondjango@users.noreply.github.com>
2023-07-12 17:31:27 +0500

    Update models.py

--- blogapp/models.py
+++ blogapp/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
     published = PublishedManager()
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.publish.year,
+        return reverse("blogapp:post_detail", args=[self.publish.year,
                                                  self.publish.month,
                                                  self.publish.day,
                                                  self.slug])
